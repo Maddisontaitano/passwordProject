00f775043f2604be45812161892102d3318d8524
Maddison Taitano <maddisontaitano@gmail.com>
2020-11-07 16:43:15 -0500

    added functions to the botton and to generate password.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var lowerCasedCharacters= ["a","b","c","d","e","f","g","h","i","j","k","l","m","
 var upperCasedCharacters= lowerCasedCharacters.map[ x=> x.toUpperCase()];
 var numbers= ["0","1","2","3","4","5","6","7","8","9"]
 
-function generatePassword(){
+function generatepassword(){
     var passLength= parseInt(prompt("How many characters would you like in your password?")) ;
 
 
@@ -34,21 +34,51 @@ function generatePassword(){
         hasUpperCase: hasUpperCase,
         hasSpecialCharacters: hasSpecialCharacters
     };
+    return password;
+
+
+     
+    var password= '';
     for( i=0; i<passLength; i++){
         if(hasLowerCase && password.length < passLength){
-            password += getRandom(lowerCasedCharacters);
+           password += getRandom(lowerCasedCharacters);
         }
         if(hasUpperCase && password.length < passwordLength){
             pasword +=getRandom(upperCasedCharacters);    
         }
         if(hasNumbers && password.length < passwordLength){
-            password +=getRandom(numericCharacters); 
-         }
+           password +=getRandom(numericCharacters); 
+        }
         if(hasSpecialCharacters && password.length < passwordlength){
-            password +=getRandom(specialCharacters);
-        }   
+           password +=getRandom(specialCharacters);
+       }   
     }  
-} 
+    return password;
+
+}
+    var button= document.querySelector("#generate")
+
+
+    // function to click on generate button 
+    // function to write the password 
+
+    function getRandom(array){
+        var position= Math.floor(Math.random() * array.length) ;
+        var arrayIndex= array[position] ;
+        return arrayIndex ;
+       }
+    
+       function writePassword(){
+           var password = generatepassword();
+           var passwordText= document.querySelector("#password") 
+
+           passwordText.value= password;
+           
+       }
+
+button.addEventlistener("click", writePassword);
+
+
 
 
  
